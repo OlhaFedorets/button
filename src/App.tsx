72ef28863dfe55343fa8b2b8ade318849972a69c
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-11 16:51:59 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {MouseEvent} from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 function App() {
     // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
@@ -22,6 +23,19 @@ function App() {
     //     console.log(num)
     // }
 
+    const Button1Foo = (subscriber: string, age: number, address: string) => {
+        console.log(subscriber, age, address)
+    }
+
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Foo = () => {
+        console.log('I am stupid button')
+    }
+
+
     return (
         <div className="App">
             {/*<button onClick={(event)=>{console.log('Hello!')}}>MyYouTubeChanel-1</button>*/}
@@ -31,8 +45,11 @@ function App() {
             {/*<button onClick={foo1}>1</button>*/}
             {/*<button onClick={()=>foo2(100200)}>2</button>*/}
 
-            <button>MyYouTubeChanel-1</button>
-            <button>MyYouTubeChanel-2</button>
+            {/* eslint-disable-next-line react/jsx-no-undef */}
+
+            <Button name={'MyYouTubeChanel-1'} callBack={()=>Button1Foo('Hello! I am Vasia!', 21, 'Live in Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={()=>Button2Foo('Hello! I am Ivan!')}/>
+            <Button name={'Stupid Button'} callBack={Button3Foo}/>
 
         </div>
     );
