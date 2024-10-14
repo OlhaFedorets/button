2c62ab5b54c8aa02faa553c47da50b900165f1b4
OlhaFedorets <fedoretsolha@gmail.com>
2024-10-14 16:47:35 +0300

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {MouseEvent} from 'react';
 import './App.css';
 import {Button} from "./components/Button";
+import {useState} from "react";
 
 // function App() {
 //
@@ -28,13 +29,43 @@ import {Button} from "./components/Button";
 
 
 
+
+
+
+// function App() {
+//
+//     const ButtonFoo = (id: number, subscriber?: string, age?: number, address?: string) => {
+//         if (id === 1) {
+//             console.log(subscriber, age, address)
+//         } else if (id === 2) {
+//             console.log(subscriber)
+//         } else {
+//             console.log('I am Stupid Button')
+//         }
+//     }
+//
+//
+//
+//     return (
+//         <div className="App">
+//
+//             <Button name={'MyYouTubeChanel-1'} callBack={()=>ButtonFoo(1, 'Hello! I am Vasia!', 21, 'Live in Minsk')}/>
+//             <Button name={'MyYouTubeChanel-2'} callBack={()=>ButtonFoo(2,  'Hello! I am Ivan!')}/>
+//             <Button name={'Stupid Button'} callBack={()=>ButtonFoo(3, 'Stupid Button')}/>
+//
+//         </div>
+//     );
+// }
+
+
+
 function App() {
 
-    // const [buttons, setButtons] = useState([
-    //     {id: 1, subscriber: 'Hello! I am Vasia!', age: 21, address: 'Live in Minsk'},
-    //     {id: 2, subscriber: 'Hello! I am Ivan!'},
-    //     {id: 3}
-    //     ]);
+    const [buttons, setButtons] = useState([
+        {id:1, name: 'MyYouTubeChanel-1', subscriber: 'Hello! I am Vasia!', age: 21, address: 'Live in Minsk'},
+        {id:1, name: 'MyYouTubeChanel-2', subscriber: 'Hello! I am Ivan!'},
+        {id:1, name: 'Stupid Button'}
+        ]);
 
     const ButtonFoo = (id: number, subscriber?: string, age?: number, address?: string) => {
         if (id === 1) {
@@ -46,14 +77,15 @@ function App() {
         }
     }
 
-
-
-    return (
+       return (
         <div className="App">
 
-            <Button name={'MyYouTubeChanel-1'} callBack={()=>ButtonFoo(1, 'Hello! I am Vasia!', 21, 'Live in Minsk')}/>
-            <Button name={'MyYouTubeChanel-2'} callBack={()=>ButtonFoo(2,  'Hello! I am Ivan!')}/>
-            <Button name={'Stupid Button'} callBack={()=>ButtonFoo(3, 'Stupid Button')}/>
+            {buttons.map(el => {
+                return (
+                    <Button key={el.id} name={el.name} callBack={()=>ButtonFoo(el.id, el.subscriber, el.age, el.address)}/>
+                    )
+
+            })}
 
         </div>
     );
